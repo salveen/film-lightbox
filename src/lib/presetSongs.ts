78ece6eaf6df7550bdb8e007d808df25331bcf0c
mrsalveen <mrsalveen@gmail.com>
2026-05-24 19:18:27 +0200

    adding more than u know song

--- src/lib/presetSongs.ts
+++ src/lib/presetSongs.ts
@@ -7,4 +7,5 @@ export const PRESET_SONGS: PresetSong[] = [
 	{ label: 'Sleep On The Floor | The Lumineers', url: 'https://www.youtube.com/watch?v=SkTZu9oPN6E' },
 	{ label: 'Riptide | Vance Joy', url: 'https://www.youtube.com/watch?v=uJ_1HMAGb4k' },
 	{ label: 'Mystery of Love | Sufjan Stevens', url: 'https://www.youtube.com/watch?v=4WTt69YO2VI' },
+	{ label: 'More than You Know | Axwell /\ Ingrosso', url: 'https://www.youtube.com/watch?v=GsF05B8TFWg' },
 ];
